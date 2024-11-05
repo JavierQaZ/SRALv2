af05d8013023d702aaa6659dae82b2f66751ae93
JavierQaZ <xjavierqazx@gmail.com>
2024-11-04 22:25:48 -0300

    more minor fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
 import Home from "./views/Home.jsx"
 import Login from "./views/Login.jsx"
-import SignUp from "./views/SignUp.jsx";
 import Recover from "./views/Recover.jsx";
 
 function App() {
@@ -21,10 +20,6 @@ function App() {
             <Route
                 path = "/home/*"
                 element = { <Home/> }/>
-
-            <Route
-                path = "/signup"
-                element = { <SignUp/> }/>
 
             <Route
                 path = "/recover"
