e19a97f3c43d01b21c23b017dfd5fd2be564d0d0
JavierQaZ <xjavierqazx@gmail.com>
2024-11-06 19:13:03 -0300

    base vista Informes

--- Frontend/src/views/Informes.jsx
+++ Frontend/src/views/Informes.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Informes(){
+    const [nombreInforme, setNombreInforme] = useState("");
+
+    const handleOnChangeNombreInforme = (e) => {
+        setNombreInforme(e.target.value)
+    }
+
+    const handleSubmitInforme = (e) => {
+
+    }
+
+
+
     return(
         <>
             <h4 className="bg-payne-grey content-title shadow">INFORMES</h4>
             <div className='content-body'>
                 <div className='container'>
+                    <form onSubmit={handleSubmitInforme}>
+                        <label className="form-label mt-3">Informe</label>
+                        <br/>
+                        <select
+                            className="custom-select"
+                            id='inlineFormCustomSelectPref'
+                            value={nombreInforme}
+                            onChange={handleOnChangeNombreInforme}
+                        >
+                            <option value='-1'> Seleccione el Informe</option>
+                        </select>
+                    </form>
+                    <hr/>
                     
                 </div>
             </div>
