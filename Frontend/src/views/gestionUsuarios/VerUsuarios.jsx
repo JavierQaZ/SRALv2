e8a992ba6e70ecc88908affe9e9a39b6542de31e
JavierQaZ <xjavierqazx@gmail.com>
2024-12-06 20:04:33 -0300

    verUsuarios

--- Frontend/src/views/gestionUsuarios/VerUsuarios.jsx
+++ Frontend/src/views/gestionUsuarios/VerUsuarios.jsx
@@ -1,8 +1,47 @@
-import React from  'react'
+import React, { useState } from  'react'
+import axios from 'axios'
 
 function VerUsuarios(){
+    const [datos, setDatos] = useState([])
+
+    axios.get(`http://localhost:5000/`)
+            .then((response) => {
+                if (response.data && response.data.datos) {
+                    setDatos(response.data.datos);
+                } else {
+                    setDatos([]);
+                    console.error('Error al obtener los datos: ', response.data);
+                }
+            })
+            .catch((error) => {
+                console.log("Error al obtener los datos", error);
+            });
+
+
     return (
-        <p>ver users</p>
+        <>
+            <h4>Visualización de Usuarios</h4>
+            <div>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>RUT</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {datos.map((item, index) => (
+                            <tr key = {index}>
+                                <td>{item.rut_usuario}</td>
+                                <td>{item.nombre_usuario} {item.apellidos_usuario}</td>
+                                <td>{item.email_usuario}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
